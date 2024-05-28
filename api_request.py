c8e4dc69f83dccbfd41ca1dc20e989bb26f452cb
1GuidoVanRossum <47911526+1GuidoVanRossum@users.noreply.github.com>
2024-05-28 23:21:46 +0300

    Update api_request.py

--- api_request.py
+++ api_request.py
@@ -9,9 +9,9 @@ def get_token():
     }
 
     data = {
-        "username": "Fora23",
-        "password": "4BOr%40B0r%40",
-        "api_token": "78c873edb46d4be1ca6f79cc6b20552deed41a13"
+        "username": "",
+        "password": "",
+        "api_token": ""
     }
 
     response = requests.post(url, headers=headers, json=data)
